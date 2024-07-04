d44e0626a813671dd432c1097a2d22b793f348d3
Aditya Kandele <kandele.aditya@gmail.com>
2024-07-04 19:26:44 +0200

    Prettier setup (#6)
    * prettier and eslint config
    
    * eslint with prettier
    
    * prettier config
    
    * prettifyall files using prettier
    
    * reverting eslint for now
    
    * nextjs upgrade for next lint

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,57 +1,53 @@
-import { auth } from "@/auth";
+import { auth } from '@/auth';
 
-import { privateRoutes, 
-    authRoutes, 
-    DEFAULT_REDIRECT_LOGIN_URL, 
-    DEFAULT_REDIRECT_HOME_URL } from '@/lib/routes';
+import { privateRoutes, authRoutes, DEFAULT_REDIRECT_LOGIN_URL, DEFAULT_REDIRECT_HOME_URL } from '@/lib/routes';
 
 export default auth((req) => {
-    const url = req.nextUrl;
-    const route = req.nextUrl.pathname;
-    
-    const isLoggedIn = !!req.auth;
-    
-    //console.log(route);
-    
-    function checkAuthRoute(authRoute: string) {
-        if(route.startsWith(authRoute)) {
-        return true;
-        }
-        return;
-    }
-    
-    //console.log(!!authRoutes.filter(checkAuthRoute).length);
-    
-    if(!!authRoutes.filter(checkAuthRoute).length) {
-        if(isLoggedIn) {
-        return Response.redirect(new URL(DEFAULT_REDIRECT_HOME_URL, url));
-        } 
-        return;    
-    }
+  const url = req.nextUrl;
+  const route = req.nextUrl.pathname;
 
-    // Typescript does not supprt spread operator as of now
-    function startsWithAny(str: string, prefixes: string[]): boolean {
-        return prefixes.some(prefix => str.startsWith(prefix));
-      }
-    
-    if(startsWithAny(route, authRoutes)) {
-        if(isLoggedIn) {
-        return Response.redirect(new URL(DEFAULT_REDIRECT_HOME_URL, url));
-        } 
-        return;       
+  const isLoggedIn = !!req.auth;
+
+  //console.log(route);
+
+  function checkAuthRoute(authRoute: string) {
+    if (route.startsWith(authRoute)) {
+      return true;
     }
-    
-    if(privateRoutes.includes(route)) {
-        if(!isLoggedIn) {
-        return Response.redirect(new URL(DEFAULT_REDIRECT_LOGIN_URL, url));
-        }
-        return;
-    }
-    
     return;
-    
-    })
+  }
+
+  //console.log(!!authRoutes.filter(checkAuthRoute).length);
+
+  if (!!authRoutes.filter(checkAuthRoute).length) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_REDIRECT_HOME_URL, url));
+    }
+    return;
+  }
+
+  // Typescript does not supprt spread operator as of now
+  function startsWithAny(str: string, prefixes: string[]): boolean {
+    return prefixes.some((prefix) => str.startsWith(prefix));
+  }
+
+  if (startsWithAny(route, authRoutes)) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_REDIRECT_HOME_URL, url));
+    }
+    return;
+  }
+
+  if (privateRoutes.includes(route)) {
+    if (!isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_REDIRECT_LOGIN_URL, url));
+    }
+    return;
+  }
+
+  return;
+});
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
